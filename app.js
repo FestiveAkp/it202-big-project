9efb76128a6b13bb924047068261aa4dae9b68c1
Akash Pillai <akashp807@gmail.com>
2020-04-28 12:39:44 -0500

    Add basic page elements for search and camera views

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const hideViews = () => {
 }
 
 
+// Function that handles site navigation
+const routeTo = route => {
+    hideViews();
+    document.querySelector('div.view#' + route).classList.add('active');
+    console.log('going to: ' + route);
+}
+
+
 // Instantiate app drawer and add hamburger event listener
 const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const hamburger = document.querySelector('.mdc-top-app-bar__navigation-icon--unbounded');
@@ -14,16 +22,50 @@ hamburger.addEventListener('click', () => drawer.open = true);
 
 
 // Show search view on entry
-document.querySelector('div.view#search').classList.add('active');
+routeTo('search');
 
 
 // Add navigation between pages using drawer buttons
 document.querySelectorAll('nav.mdc-list>a.mdc-list-item').forEach(button => {
     button.addEventListener('click', () => {
-        hideViews();
-        const destination = button.getAttribute('href');
-        document.querySelector(destination).classList.add('active');
+        routeTo(button.children[1].textContent.toLowerCase())
         drawer.open = false;
-        console.log('going to: ' + destination);
     });
+});
+
+
+// Instantiate search button and search bar and add navigation to result view
+const searchBtn = document.querySelector('#search-button');
+const searchBar = document.querySelector('.mdc-text-field')
+new mdc.textField.MDCTextField(searchBar);
+
+searchBtn.addEventListener('click', () => {
+    const searchValue = document.querySelector('#search-input').value;
+
+    if (searchValue !== '') {
+        routeTo('result');
+    }
+});
+
+// If user presses enter on search bar, click the search button
+searchBar.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        searchBtn.click();
+    }
+});
+
+
+// Have all back buttons on the site redirect to the search page
+document.querySelectorAll('#back-button').forEach(button => {
+    button.addEventListener('click', () => {
+        routeTo('search');
+        document.querySelector('#search-input').value = '';
+    });
+});
+
+
+// Instantiate camera button and add navigation to camera view
+const cameraBtn = document.querySelector('#camera-button');
+cameraBtn.addEventListener('click', () => {
+    routeTo('camera');
 });
