d97a694583f30b9ba0944c8496f2cd662f8a27db
Akash Pillai <akashp807@gmail.com>
2020-04-30 22:30:46 -0500

    Show dictionary results and add fancy styling

--- dict.js
+++ dict.js
@@ -11,11 +11,59 @@ async function queryDictionary(word) {
     });
     let data = await response.json();
     console.log(data);
+    return data;
+}
+
+
+function updateUI(result) {
+    document.querySelector('#word-title').textContent = result.word;
+    
+    const pronunciation = document.querySelector('#pronunciation');
+    pronunciation.textContent = `[ ${result.pronunciation.all || result.pronunciation} ]`;
+    pronunciation.classList.add('mdc-typography--subtitle1');
+
+    const syllables = document.querySelector('#syllables');
+    syllables.textContent = result.syllables.list.join('\u00B7');
+    syllables.classList.add('mdc-typography--subtitle1');
+    
+
+    document.querySelector('#syllables').textContent 
+
+    result.results.forEach(definition => {
+        const listingPartOfSpeech = document.createElement('span');
+        listingPartOfSpeech.textContent = definition.partOfSpeech;
+        listingPartOfSpeech.classList.add('part-of-speech');
+        
+        const listingDefinition = document.createElement('span');
+        listingDefinition.textContent = " - " + definition.definition;
+        listingDefinition.classList.add('definition');
+        
+        const listing = document.createElement('li');
+        listing.classList.add('definition', 'mdc-typography--body1');
+        listing.append(listingPartOfSpeech, listingDefinition);
+        
+        // If the definition has an example, show it
+        if (definition.examples != null) {
+            const exampleListing = document.createElement('li');
+            exampleListing.textContent = definition.examples[0];
+            exampleListing.classList.add('example', 'mdc-typography--body2');
+            
+            const exampleList = document.createElement('ul');
+            exampleList.appendChild(exampleListing);
+    
+            listing.appendChild(exampleList);
+        }
+
+        // Add definition to list
+        document.querySelector('#definitions').appendChild(listing);
+    });
 }
 
 
 // When the search button is clicked, query the API for that search term
 document.querySelector('#search-button').addEventListener('click', () => {
     const searchValue = document.querySelector('#search-input').value;
-    queryDictionary(searchValue);
+    
+    queryDictionary(searchValue)
+        .then(result => updateUI(result));
 });
